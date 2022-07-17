3123a7bf485e50f23c0fadde6c48f6daa2e636fd
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-17 18:14:16 +0700

    #17 - Bug Fix ActorRepoJPA :bug: :wrench:

--- src/main/java/com/hendisantika/repository/ActorRepoJPA.java
+++ src/main/java/com/hendisantika/repository/ActorRepoJPA.java
@@ -2,6 +2,7 @@ package com.hendisantika.repository;
 
 import com.hendisantika.model.Actor;
 import com.hendisantika.model.Movie;
+import com.hendisantika.model.User;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -19,38 +20,49 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 @Repository("actorRepository")
-public interface ActorRepoJPA implements ActorRepository {
+public class ActorRepoJPA implements ActorRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
-    void create(Actor actor) {
+    public void create(Actor actor) {
         entityManager.persist(actor);
     }
 
     @Override
-    Actor update(Actor actor) {
+    public Actor update(Actor actor) {
         return entityManager.merge(actor);
     }
 
     @Override
-    void delete(Long actor_id) {
+    public void delete(Long actor_id) {
         entityManager.remove(entityManager.getReference(Actor.class, actor_id));
     }
 
     @Override
-    List<Actor> findActors() {
+    public List<Actor> findActors() {
         return entityManager.createQuery("from Actor", Actor.class).getResultList();
     }
 
     @Override
-    Actor findActorById(Long id) {
+    public Actor findActorById(Long id) {
         return entityManager.find(Actor.class, id);
     }
 
     @Override
-    List<Actor> findActorsByMovie(Movie movie) {
+    public List<Actor> findActorsByMovie(Movie movie) {
         String q = "SELECT * FROM Movie_Actors WHERE movie_id = " + movie.getId();
         return entityManager.createNativeQuery(q).getResultList();
+    }
+
+    // This is irrelevant to the actor model however it was easier to implement this
+    // method inside an existing repository class rather than creating a new one
+    // just for one method.
+    @Override
+    public void registerUser(User user) {
+        String q = "INSERT INTO users(username, password, enabled) VALUES('" + user.getUsername() + "', '{noop}" + user.getPassword() + "', " + user.isEnabled() + ");";
+        String q2 = "INSERT INTO authorities(username, authority) VALUES('" + user.getUsername() + "', '" + user.getRole() + "');";
+        entityManager.createNativeQuery(q).executeUpdate();
+        entityManager.createNativeQuery(q2).executeUpdate();
     }
 }
