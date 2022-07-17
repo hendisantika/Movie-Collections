186636441ece51ba40885ac5e5b37fee9a230522
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-18 05:37:51 +0700

    #49 - Add LoginController :pencil2: :green_book:

---
+++ src/main/java/com/hendisantika/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.AppService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Movie-Collections
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/18/22
+ * Time: 05:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final AppService appService;
+}
