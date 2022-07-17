9a6db5b82fa4b3257c0cd63212206951311e20ad
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-18 05:55:28 +0700

    #82 - Add configure in SecurityConfiguration :pencil2: :green_book:

--- src/main/java/com/hendisantika/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/config/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.hendisantika.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -24,4 +26,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
 
     private final DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**/favicon.ico", "/css/**", "/js/**", "/images/**", "/webjars/**", "/login", "/register").permitAll()
+                .antMatchers("/movies/list").access("hasRole('USER')")
+                .antMatchers("/movies/edit/**", "/movies/cast", "/movies/new/**", "/movies/update/**", "/movies/delete/**").access("hasRole('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
 }
