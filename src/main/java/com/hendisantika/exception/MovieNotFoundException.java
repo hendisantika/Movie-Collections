54a40cf781aa7ed77e8a1ad00c7742d94beb0f44
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-17 18:02:22 +0700

    #3 - Add MovieNotFoundException

---
+++ src/main/java/com/hendisantika/exception/MovieNotFoundException.java
@@ -0,0 +1,19 @@
+package com.hendisantika.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Movie-Collections
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/17/22
+ * Time: 18:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class MovieNotFoundException extends RuntimeException {
+
+    public MovieNotFoundException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+}
