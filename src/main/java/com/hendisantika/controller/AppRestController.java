70429e9df4ce1fd19552f93558c270176f54f5a9
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-18 05:45:18 +0700

    #67 - Add getMovie in AppRestController :pencil2: :green_book:

--- src/main/java/com/hendisantika/controller/AppRestController.java
+++ src/main/java/com/hendisantika/controller/AppRestController.java
@@ -1,10 +1,13 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.exception.MovieNotFoundException;
 import com.hendisantika.model.Movie;
 import com.hendisantika.service.AppService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +34,17 @@ public class AppRestController {
     public ResponseEntity<List<Movie>> getMovies() {
         List<Movie> movies = appService.findMovies();
         return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping(value = "/movie/{id}")
+    public ResponseEntity<Movie> getMovie(@PathVariable("id") Long id) {
+        try {
+            Movie movie = appService.findMovie(id);
+            return ResponseEntity.ok(movie);
+        } catch (MovieNotFoundException ex) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
