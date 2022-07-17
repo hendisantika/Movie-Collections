e9257abc78748a62f5e407b7abe99fd5550e1856
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-17 18:06:47 +0700

    #7 - Add Movie Model

---
+++ src/main/java/com/hendisantika/model/Movie.java
@@ -0,0 +1,72 @@
+package com.hendisantika.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Movie-Collections
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/17/22
+ * Time: 18:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "t_movies")
+@JsonIgnoreProperties(value = {"castRef"})
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "release_date")
+    private Date date;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "rating")
+    private float rating;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "movie_actors",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
+    private Set<Actor> cast = new HashSet<>();
+
+}
