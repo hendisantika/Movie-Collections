8be1b830e3013eb8948f3264f4c52a3aaaa4519a
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-18 05:38:48 +0700

    #52 - Add registerConfirm in LoginController :pencil2: :green_book:

--- src/main/java/com/hendisantika/controller/LoginController.java
+++ src/main/java/com/hendisantika/controller/LoginController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,5 +34,17 @@ public class LoginController {
     public String register(Model model) {
         model.addAttribute("user", new User());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerConfirm(@ModelAttribute User user, Model model) {
+        if (user.getUsername().length() <= 0 || user.getPassword().length() <= 0) {
+            return "redirect:/register";
+        } else {
+            user.setEnabled(true);
+            appService.registerUser(user);
+            return "login";
+        }
+
     }
 }
