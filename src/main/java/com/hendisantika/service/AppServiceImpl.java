d6e3ac1090f601b1b2c918250a2a8a492bd0355d
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-17 18:23:38 +0700

    #29 - Add AppServiceImpl :bug: :wrench:

---
+++ src/main/java/com/hendisantika/service/AppServiceImpl.java
@@ -0,0 +1,27 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.ActorRepository;
+import com.hendisantika.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Movie-Collections
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/17/22
+ * Time: 18:23
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AppServiceImpl implements AppService {
+
+    private MovieRepository movieRepository;
+    private ActorRepository actorRepository;
+
+}
