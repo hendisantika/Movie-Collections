29683c09800edb84271f1f359c98d158ccab47bc
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-17 17:59:52 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/MovieCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCollectionsApplication.class, args);
+    }
+
+}
